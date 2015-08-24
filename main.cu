c054874db67a8525c9968f316a7e628e962358e3
Dmitry Lyakh <quant4me@gmail.com>
2015-08-24 17:20:48 -0400

    Ready to try new benchmarking.

--- main.cu
+++ main.cu
@@ -26,7 +26,7 @@ size_t buf_size=1000000; //desired argument buffer size in bytes (Host pinned RA
 const double init_val=0.01; //some initialization value for tensor blocks
 
 
-void print_task_timings(int ntasks, const cudaTask_t * tsks, const int * tsk_stats, int first_task=0){
+void print_task_timings(int ntasks, cudaTask_t * tsks[], const int * tsk_stats, int first_task=0){
  int i;
  float tm,incopy,outcopy,comput;
 //Print timings (total time, H2D time, D2H time, GPU computing time):
@@ -65,27 +65,33 @@ int main(int argc, char** args){
   -1, 1,-1, 2, 3, 4, 0, 0, //Contraction pattern 7
   -4, 4, 3, 2, 1,-1, 0, 0  //Contraction pattern 8
  };
- double cntr_flops[8];
+ double cflops[8];
 
 //Set the number of Flops for each tensor contraction:
- 
+ cflops[0]=dims[0][0]*dims[0][1]*dims[0][2]*dims[0][3]*dims[1][0]*dims[1][1];            //Flops for tensor contraction 1
+ cflops[1]=cflops[0];                                                                    //Flops for tensor contraction 2
+ cflops[2]=dims[6][0]*dims[6][1]*dims[6][2]*dims[6][3]*dims[6][4]*dims[6][5]*dims[7][0]; //Flops for tensor contraction 3
+ cflops[3]=cflops[2];                                                                    //Flops for tensor contraction 4
+ cflops[4]=dims[12][0]*dims[12][1]*dims[13][0]*dims[13][1]*dims[13][2];                  //Flops for tensor contraction 5
+ cflops[5]=cflops[4];                                                                    //Flops for tensor contraction 6
+ cflops[6]=dims[18][0]*dims[18][1]*dims[18][2]*dims[18][3]*dims[19][0];                  //Flops for tensor contraction 7
+ cflops[7]=cflops[6];                                                                    //Flops for tensor contraction 8
 
 //Allocate argument buffers (initializes both the Host and all GPUs):
  errc=arg_buf_allocate(&buf_size,&max_args,first_gpu,last_gpu); //if you don't want to use Host buffer, set buf_size to a small value
  if(errc){printf("#ERROR: arg_buf_allocate failed!"); return 1;};
  printf("Allocated argument buffers: Host buffer size = %zu bytes: max args = %d \n",buf_size,max_args);
 
-//Check GPUs:
+//Check all GPUs:
  for(i=first_gpu;i<=last_gpu;i++){
   errc=gpu_activate(i); if(errc){printf("#ERROR: gpu_activate %d failed!",i); return 1;};
   printf("Activated GPU %d \n",i);
  };
 
 //Create and initialize 24 tensor blocks:
- //Tensor blocks [0-5] on first gpu:
  for(j=1;j<=8;j++){ //eight tensor contractions
-  my_gpu=(j-1)/2; //expect 4 GPUs
-  for(i=(j-1)*3;i<j*3;i++){
+  my_gpu=first_gpu+(j-1)/2; //expecting 4 GPUs
+  for(i=(j-1)*3;i<j*3;i++){ //tensor block number
    errc=tensBlck_create(&(tb[i])); //create an empty tensor block
    if(errc){printf("#ERROR: tensBlck_create %d failed!",i); return 1;};
    errc=tensBlck_alloc(tb[i],my_gpu,R8,rank[i],&(dims[i][0])); //allocate memory on Host (pinned) and GPU (buffer), init to zero on Host
@@ -95,17 +101,15 @@ int main(int argc, char** args){
  };
 
 //Create empty CUDA task handles:
- for(i=0;i<MAX_TASKS;i++){
-  errc=cuda_task_create(&(tsks[i])); if(errc){printf("#ERROR: cuda_task_create %d failed! \n",i); return 1;};
- };
+ for(i=0;i<MAX_TASKS;i++){errc=cuda_task_create(&(tsks[i])); if(errc){printf("#ERROR: cuda_task_create %d failed! \n",i); return 1;};};
  printf("Created %d empty task handles \n",MAX_TASKS);
-//-----------------------------------------------------------------------------------------------------------
-//Tensor operations (examples):
+//----------------------------------------------------------
+//TENSOR OPERATIONS (examples):
 
 //Execute tensor initializations (concurrent tasks are executed on GPUs asynchronously w.r.t. Host):
  k=0;
  for(j=1;j<=8;j++){ //tensor contraction number
-  i=(j-1)*3;
+  i=(j-1)*3; //destination tensor block number
 //  errc=gpu_tensor_block_init_(tb[i],0.0,COPY_BACK,tsks[k++]); if(errc){printf("#ERROR: gpu_tensor_block_init_ D %d failed!\n",i); return 1;};
   errc=gpu_tensor_block_init_(tb[i+1],init_val,COPY_BACK,tsks[k++]); if(errc){printf("#ERROR: gpu_tensor_block_init_ L %d failed!\n",i); return 1;};
   errc=gpu_tensor_block_init_(tb[i+2],init_val,COPY_BACK,tsks[k++]); if(errc){printf("#ERROR: gpu_tensor_block_init_ R %d failed!\n",i); return 1;};
@@ -115,19 +119,19 @@ int main(int argc, char** args){
  print_task_timings(k,tsks,tsk_stats); //print timings for all tasks
 
 //Clean CUDA tasks for reuse:
- for(i=0;i<k;i++){errc=cuda_task_clean(tsk[i]); if(errc){printf("#ERROR: cuda_task_clean %d failed!",i); return 1;};};
+ for(i=0;i<k;i++){errc=cuda_task_clean(tsks[i]); if(errc){printf("#ERROR: cuda_task_clean %d failed!",i); return 1;};};
 
-//Mark tensor blocks as absent on GPU:
+//Mark tensor blocks as absent on GPU (to enforce PCIe copy):
  for(i=0;i<24;i++){errc=tensBlck_set_absence(tb[i]);};
 
-//Choose the tensor transpose algorithm:
- gpu_set_transpose_algorithm(EFF_TRN_OFF); //turn off the efficient tensor transpose
- gpu_set_matmult_algorithm(BLAS_OFF); //switch to custom BLAS kernels
+//Choose the tensor transpose and matrix multiplication algorithms:
+ gpu_set_transpose_algorithm(EFF_TRN_OFF); //turn off the efficient tensor transpose (slow)
+ gpu_set_matmult_algorithm(BLAS_OFF); //switch to custom BLAS kernels (slow)
 
 //Execute tensor contractions (concurrent tasks are executed on GPUs asynchronously w.r.t. Host):
  tms=clock(); k=0;
  for(j=1;j<=8;j++){ //eight tensor contractions
-  i=(j-1)*3;
+  i=(j-1)*3; //destination tensor block for each tensor contraction
   errc=gpu_tensor_block_contract_dlf_(&(cptrn[j-1][0]),tb[i+1],tb[i+2],tb[i],COPY_BACK,tsks[k++]);
   if(errc){printf("#ERROR: gpu_tensor_block_contract_ %d failed!",j); return 1;};
  };
@@ -135,11 +139,11 @@ int main(int argc, char** args){
  tme=clock(); tmm=((double)(tme-tms))/CLOCKS_PER_SEC;
  if(errc){printf("#ERROR: cuda_tasks_wait for contractions failed!"); return 1;};
  print_task_timings(k,tsks,tsk_stats); //print timings for all tasks
+//Print the cumulative GFlop/s (all contractions on all GPUs):
+ flops=0.0; for(i=0;i<8;i++){flops+=cflops[i];} //total number of Flops
+ printf("Cumulative GFlop/s = %f \n",flops/(tmm*1073741824.0));
 
- printf("Effective GFlop/s  = %f \n",flops/(tmm*1073741824.0)); //single tensor contractions
- printf("On-GPU GFlop/s     = %f \n",flops/(comput*1073741824.0));
-
-// Inspect results:
+//Inspect the results:
  for(i=0;i<24;i+=3){printf("Destination element inspection %d: %e \n",i,((double*)(tb[i]->elems_h))[13]);};
 //---------------------------------------------------------------------------------------------------------
 
