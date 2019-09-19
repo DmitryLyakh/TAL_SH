b898ec712335661f043e772aa862f20df4cc8358
Dmitry I. Lyakh <quant4me@gmail.com>
2019-09-19 16:11:02 -0400

    Fixed CMAKE file.

--- tensor_method.hpp
+++ tensor_method.hpp
@@ -1,5 +1,5 @@
-/** TAL-SH: Tensor Method Interface
-REVISION: 2019/09/13
+/** TAL-SH: Tensor Method Interface (Tensor Functor)
+REVISION: 2019/09/18
 
 Copyright (C) 2018-2019 Dmitry I. Lyakh (Liakh)
 Copyright (C) 2018-2019 Oak Ridge National Laboratory (UT-Battelle) **/
@@ -11,41 +11,28 @@ Copyright (C) 2018-2019 Oak Ridge National Laboratory (UT-Battelle) **/
 
 #include "byte_packet.h"
 
-#include <vector>
+namespace talsh{
 
-//Dense locally stored tensor block for ExaTN:
-struct TensorDenseBlock{
- std::vector<std::size_t> base;
- std::vector<std::size_t> dims;
- void * body;
- int data_kind;
+class Tensor;
 
- TensorDenseBlock(): body(nullptr), data_kind(NO_TYPE){}
- ~TensorDenseBlock(){if(body) free(body); body = nullptr;}
-
- inline std::size_t getVolume(){
-  std::size_t vol = 1;
-  for(unsigned int i = 0; i < dims.size(); ++i) vol *= dims[i];
-  return vol;
- }
-};
-
-//External tensor method (identifiable):
+//External tensor functor (identifiable):
 template <typename IdentifiableConcept>
-class TensorMethod: public IdentifiableConcept{
+class TensorFunctor: public IdentifiableConcept{
 public:
 
- TensorMethod() = default;
- virtual ~TensorMethod() = default;
+ TensorFunctor() = default;
+ virtual ~TensorFunctor() = default;
 
  //Packing/unpacking data members into/from a plain byte packet:
  virtual void pack(BytePacket & packet) = 0;
  virtual void unpack(BytePacket & packet) = 0;
 
  //Application-defined external tensor method:
- virtual int apply(TensorDenseBlock & local_tensor) = 0;
+ virtual int apply(Tensor & local_tensor) = 0;
 
 };
+
+} //namespace talsh
 
 #endif //EXATN_SERVICE
 
