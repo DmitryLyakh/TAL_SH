89c90a5e519fdfc823db2242642a34edc7530eb1
Dmitry I. Lyakh <quant4me@gmail.com>
2020-08-11 13:31:58 -0400

    Fixed number of levels in the GPU memory buffers.

--- mem_manager.cpp
+++ mem_manager.cpp
@@ -46,10 +46,10 @@ FOR DEVELOPERS ONLY:
 #define MEM_ALIGN GPU_CACHE_LINE_LEN //memory alignment (in bytes) for argument buffers
 //Host argument buffer structure (adjust TALSH_NO_HOST_BUFFER in talsh.h as well):
 #define BLCK_BUF_DEPTH_HOST 13       //number of distinct tensor block buffer levels on Host
-#define BLCK_BUF_TOP_HOST 6          //number of argument buffer entries of the largest size (level 0) on Host: multiple of 3
+#define BLCK_BUF_TOP_HOST 3          //number of argument buffer entries of the largest size (level 0) on Host: multiple of 3
 #define BLCK_BUF_BRANCH_HOST 2       //branching factor for each subsequent buffer level on Host
 //GPU argument buffer structure (the total number of entries must be less or equal to MAX_GPU_ARGS):
-#define BLCK_BUF_DEPTH_GPU 6         //number of distinct tensor block buffer levels on GPU
+#define BLCK_BUF_DEPTH_GPU 5         //number of distinct tensor block buffer levels on GPU
 #define BLCK_BUF_TOP_GPU 6           //number of argument buffer entries of the largest size (level 0) on GPU: multiple of 3
 #define BLCK_BUF_BRANCH_GPU 2        //branching factor for each subsequent buffer level on GPU
 
