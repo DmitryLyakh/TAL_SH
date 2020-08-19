ce239ef0275ea9b0c9fbbd916d9a2fb07dcad3bb
Ajay Panyala <ajay.panyala@gmail.com>
2020-08-19 10:25:50 -0700

    Improved CMake build: install targets, config files, build tests

--- nvtx_profile.h
+++ nvtx_profile.h
@@ -1,9 +1,13 @@
 #ifndef NVTX_PROFILE_H_
 #define NVTX_PROFILE_H_
 
+#ifdef __cplusplus
 extern "C" {
+#endif
  void prof_push(const char * annotation, int color);
  void prof_pop();
+#ifdef __cplusplus
 }
+#endif
 
 #endif /*NVTX_PROFILE_H_*/
